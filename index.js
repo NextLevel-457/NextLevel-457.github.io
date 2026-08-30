db689763ae5684fa553c2525b3206e081e5f3161
Nexus457 <167141032+NextLevel-457@users.noreply.github.com>
2026-08-30 13:53:52 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-document.getElementById("form").addEventListener("submit", function (e) {
-  e.preventDefault();
-
-  var formData = new FormData(form);
-  // output as an object
-  console.log(Object.fromEntries(formData));
-
-  // ...or iterate through the name-value pairs
-  for (var pair of formData.entries()) {
-    console.log(pair[0] + ": " + pair[1]);
-  }
+window.addEventListener("DOMContentLoaded", (event) => {
+  document.getElementById("form").addEventListener("submit", function (e) {
+    e.preventDefault();
+  
+    var formData = new FormData(form);
+    // output as an object
+    console.log(Object.fromEntries(formData));
+  
+    // ...or iterate through the name-value pairs
+    for (var pair of formData.entries()) {
+      console.log(pair[0] + ": " + pair[1]);
+    }
+  });
 });
